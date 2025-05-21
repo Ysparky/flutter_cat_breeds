dc277a772fe14f395f2b33b68b889f514befab6b
Jose Caldas <jos.caldas.27@gmail.com>
2025-05-21 13:39:28 -0500

    feat: environment variable management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'core/theme/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_cat_breeds/core/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
